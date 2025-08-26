6611f0666dd8d12c1adea32f1e8a8b4a82501868
Alejandro Fernández Vázquez <alex2768m@gmail.com>
2025-08-26 21:22:21 +0200

    Upgraded background effect

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
 import FloatingShapes from './components/FloatingBackground';
+import SwirlBackground from './components/SwirlBackground';
 import TextTransition from './components/TextTransition';
 
 export default function Home() {
+  const SHOW_FLOATING_SHAPES = false;
   return (
-    <main className="rainbow-bg flex items-center justify-center h-screen w-screen fixed inset-0 overflow-hidden">
-      <FloatingShapes />
+    <main className="rainbow-bg hide-rainbow flex items-center justify-center h-screen w-screen fixed inset-0 overflow-hidden">
+      {/* Swirl effect background */}
+      <SwirlBackground />
+      {/* Legacy background with easy re-enable */}
+      {SHOW_FLOATING_SHAPES && <FloatingShapes />}
       <div>
         <TextTransition />
       </div>
